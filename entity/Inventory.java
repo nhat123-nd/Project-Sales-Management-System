d22fa8ddd127d7318eaae4037b54ed64e2558eae
nhat123-nd <ngonhat654@gmail.com>
2026-06-08 09:20:11 +0700

    done

--- entity/Inventory.java
+++ entity/Inventory.java
@@ -28,6 +28,40 @@ public class Inventory {
     public void setCurrentStock(int currentStock) {
         this.currentStock = currentStock;
     }
+ 
+    public boolean checkStock(int quantity){
+        return quantity > 0 && currentStock >= quantity;
+    }
+
+    public boolean reduceStock(int quantity) {
+        if (quantity <= 0) {
+            return false;
+        }
+
+        if (currentStock >= quantity) {
+            currentStock -= quantity;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void addStock(int quantity){
+        if(quantity > 0){
+            currentStock += quantity;
+        }
+    }
+
+     public void updateStock(int newStock) {
+
+        if(newStock >= 0){
+            currentStock = newStock;
+        }
+    }
+
+    public String toString(){
+        return toString.format("Product ID: " + productId + ", Stock: " + currentStock;)
+    }
     
     
 }
